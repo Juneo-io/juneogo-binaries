4eb3a721255c59d68e2cae49307f7182750d46ff
nlukic97 <nlukic97@gmail.com>
2023-11-08 15:18:41 +0100

    script clones repo, sets up binaries and service, and removes repo files

--- simple_setup.sh
+++ simple_setup.sh
@@ -2,16 +2,28 @@
 
 # Define variables
 JUNEO_NODE_DIR="$HOME/juneogo-binaries"
-JUNEO_BIN="$JUNEO_NODE_DIR/juneogo"
+JUNEO_BIN="$HOME/juneogo"
 JUNEO_SERVICE_DIR="$HOME/.config/systemd/user"
 JUNEO_SERVICE_FILE="$JUNEO_SERVICE_DIR/juneogo.service"
 PLUGINS_DIR="$HOME/.juneogo/plugins"
+BINARIES_REPO="https://github.com/Juneo-io/juneogo-binaries"
+
+# Clone the binaries repository
+echo "Cloning juneogo binaries from $BINARIES_REPO..."
+if ! git clone "$BINARIES_REPO" "$JUNEO_NODE_DIR"; then
+    echo "Error: Failed to clone juneogo binaries."
+    exit 1
+fi
+
+# mOVING binary and config file to the home directory
+mv $JUNEO_NODE_DIR/juneogo $HOME
+mv $JUNEO_NODE_DIR/config.json $HOME
 
 # Set up binaries and plugins
 chmod +x "$JUNEO_BIN"
 mkdir -p "$PLUGINS_DIR"
 if [ -d "$JUNEO_NODE_DIR/plugins" ]; then
-    cp -r "$JUNEO_NODE_DIR/plugins/"* "$PLUGINS_DIR"
+    mv "$JUNEO_NODE_DIR/plugins/"* "$PLUGINS_DIR"
     chmod +x "$PLUGINS_DIR"/*
 fi
 
@@ -40,6 +52,9 @@ EOF
 systemctl --user daemon-reload
 systemctl --user enable juneogo.service
 systemctl --user start juneogo.service
+
+rm -Rf $JUNEO_NODE_DIR
+rm $HOME/simple_setup.sh
 
 echo
 echo "Done! Your JuneoGo node setup is complete."
